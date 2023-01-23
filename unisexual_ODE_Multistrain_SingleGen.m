81af637fd97b65ebb5363c8712f1bbb22abe20f2
Xiaoyuan Liu <72927467+phil-liu1020@users.noreply.github.com>
2023-01-23 14:21:28 +0000

    Update unisexual_ODE_Multistrain_SingleGen.m

--- unisexual_ODE_Multistrain_SingleGen.m
+++ unisexual_ODE_Multistrain_SingleGen.m
@@ -6,7 +6,7 @@ function M = unisexual_ODE_Multistrain_SingleGen(alphastrains,S)
 % The dimension of vector x is (S+nchoosek(S+1 ,2),1)
 
 % S is the number of strains in the system.
-
+% The output M is an array with first row giving the row number of the array. The second and third rows give information on which strain each row of x (each ODE) contains. If a row of x represents a cell with strain 1 fused with strain 2, this corresponding row of M will contain a 1 and 2 in its 2nd and 3rd columns.   
 
 
 x = zeros(S+nchoosek(S+1 ,2),1);
