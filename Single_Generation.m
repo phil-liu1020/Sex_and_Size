7175b0d9293534d8af961d09467f4a913ffcd696
Xiaoyuan Liu <72927467+phil-liu1020@users.noreply.github.com>
2023-01-23 14:00:58 +0000

    Update Single_Generation.m

--- Single_Generation.m
+++ Single_Generation.m
@@ -7,20 +7,26 @@ function f = Single_Generation(mstrains,alphastrains,x,S,beta,C)
 Mv=unisexual_ODE_Multistrain_SingleGen(alphastrains,S);  % writes the array Mv which is used to determine what each column of x (each ODE) represents. e.g. when S=2, x(4) represents F^{1}_{2} the number of strain 1 cells that've fused with strain 2 cells.
 f=zeros(S,1);
 
-for k=1:S          % This for loop with index k calculates the fitness of each strain i.e. w_k for k in [1,S] as defined in the text.
-   wk=0;   
+for k=1:S          % This for loop with index k calculates the fitness of each strain in the system i.e. w_k for k in [1,S] as defined in the text.
+   wk=0; 
+   
+   -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+   %%%This block of code calculates the fitness of the k-th strain.
+   
+   
    Mk = Mv(any(Mv==k,2),:);        % Mk outputs an array showing which column of x (which ODE) contains strain k cells.     
-      for ii=1:length(Mk(:,1))
+      for ii=1:length(Mk(:,1))     % This for loop with index ii calculates the fitness of an individual strain.
 
-         if Mk(ii,3)==0 || Mk(ii,2)==0       % checks if the progeny is a zygote, if so we multiply by (1-C).
+         if Mk(ii,3)==0 || Mk(ii,2)==0       % checks if the ii-th column of Mk represents a zygote, if so we multiply by (1-C).
          wk=wk+x(length(x(:,1)),Mk(ii,1))*exp(-beta/( mstrains(Mk(ii,2)+1) + mstrains(Mk(ii,3)+1) ) );    % any rows with k in their 2nd or 3rd columns. the +1 is here because the first entry of mstrains is set to 0 for a purpose. If an entry in Mk is 0, then it adds mstrains(1).  
          else
          wk=wk+x(length(x(:,1)),Mk(ii,1))*exp(-beta/( mstrains(Mk(ii,2)+1) + mstrains(Mk(ii,3)+1) ) )*(1-C);
          end
       
       end 
-      w(k)=wk;                   % fitness of kth strain
+     ----------------------------------------------------------------------------------------------------------------------------------------------------------------
       
+      w(k)=wk;                   % writes the fitness of k-th strain into a vector w. 
 end
 
 for i=1:length(w)                % frequency of each strain
