669938e88524d58c94e63e5a835dee28144ac696
Xiaoyuan Liu <72927467+phil-liu1020@users.noreply.github.com>
2023-01-23 14:28:02 +0000

    Update Invasion_dynamics.m

--- Invasion_dynamics.m
+++ Invasion_dynamics.m
@@ -3,8 +3,12 @@ function gend = Invasion_dynamics(mstrains,alphastrains,S,beta,C,G,T,A,M,f)
 % This function simulates then invasion dynamics over G generations.
 % Parameters: G - number of generations
 %             f - initial frequency of each strain.
+%             mstrains and alphastrains are the mass and fusion rates of each strain.
+%             S - number of strains.
 
 g=zeros(S,1);
+
+% The for loop iterates the function "Single_Generation" over G generations. This function outputs the frequency of each strain at the G-th generation.
 
 for i=1:G
 
