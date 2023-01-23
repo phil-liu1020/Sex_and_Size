7ccb56f12ed0e4bd3f534650d411ba1751a1a8d5
Xiaoyuan Liu <72927467+phil-liu1020@users.noreply.github.com>
2023-01-23 14:36:31 +0000

    Update unisexual_ODE_Multistrain_SingleGen.m

--- unisexual_ODE_Multistrain_SingleGen.m
+++ unisexual_ODE_Multistrain_SingleGen.m
@@ -1,9 +1,4 @@
-function M = unisexual_ODE_Multistrain_SingleGen(alphastrains,S)
-
-% x is the ODE variable. x(1) to x(S) represent dN1/dt to dNS/dt, where N
-% is the number of unfertilised gametes.
-
-% The dimension of vector x is (S+nchoosek(S+1 ,2),1)
+function M = unisexual_ODE_Multistrain_SingleGen(S)
 
 % S is the number of strains in the system.
 % The output M is an array with first row giving the row number of the array. The second and third rows give information on which strain each row of x (each ODE) contains. If a row of x represents a cell with strain 1 fused with strain 2, this corresponding row of M will contain a 1 and 2 in its 2nd and 3rd columns.   
