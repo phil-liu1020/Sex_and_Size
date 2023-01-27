07da5c67b79419f77bd03bb7cd69ed91502210db
Xiaoyuan Liu <72927467+phil-liu1020@users.noreply.github.com>
2023-01-27 14:19:05 +0000

    Update fig4b_FRTI_script.m

--- Data_generation/Fig4/panel_b/fig4b_FRTI_script.m
+++ Data_generation/Fig4/panel_b/fig4b_FRTI_script.m
@@ -4,10 +4,10 @@
 A=100; M=10; T=0.1; C=0.7; beta1=4; beta2=0.01; lambda21=1/6; lambda12=13/222; mu=0.00035; delta=0.007; NEVOL=3500; f0=0.002; m0=2; alpha0=0.6; 
 switching_environments=1; plasticity=0; return_traits=0; number_of_realisations=25; alphamax=1000;
 
+addpath(genpath('Sex_and_Size-main'))
 
 [~,~,m,alpha,~,~]=Evolutionary_trajectories(number_of_realisations,m0,alpha0,A,M,T,C,beta1,beta2,lambda12,lambda21,mu,NEVOL,f0,delta,alphamax, switching_environments, plasticity, return_traits );
 
-addpath(genpath('Sex_and_Size-main'))
 
 cd ..
 
