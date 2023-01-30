2c7310af7fa5e69b948a063a6125ba71cc8d2271
Xiaoyuan Liu <72927467+phil-liu1020@users.noreply.github.com>
2023-01-30 16:01:09 +0000

    Update FigS5_to_S14_script.m

--- Figure_generation/FigS5_to_S14_script.m
+++ Figure_generation/FigS5_to_S14_script.m
@@ -1,8 +1,8 @@
 % Instructions for producing FigS5 to FigS14.
 
-% First, open "Data_Files/FigS5" folder.
+% First, open "Data_Files", then open the folder for the figure you'd like to plot.
 
-% To reproduce all the panels of FigS5, execute the command
+% To reproduce all the panels of a given figure, execute the command
   
 Evol_Branching_plots(StrainsData_m);
 
