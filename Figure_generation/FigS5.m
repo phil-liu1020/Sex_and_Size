11a2682865178063716909b6b482e9e980cb0dff
Xiaoyuan Liu <72927467+phil-liu1020@users.noreply.github.com>
2023-01-30 15:33:14 +0000

    Update and rename FigS5 to FigS5.m

---
+++ Figure_generation/FigS5.m
@@ -0,0 +1,13 @@
+% Open "Data_Files/FigS5" folder.
+
+  % To reprdouce all the panels of FigS5, execute the command
+  
+Evol_Branching_plots(StrainsData_m);
+
+figure
+
+Evol_Branching_plots(StrainsData_alpha);
+
+figure
+
+Evol_Branching_plots_Coevolution(StrainsData_alpha);
