6cc92f09d0a084aebee28dd98094739afcaf4f0e
Xiaoyuan Liu <72927467+phil-liu1020@users.noreply.github.com>
2023-01-30 10:41:02 +0000

    Update fig4a_FRTE_script.m

--- Data_generation/Fig4/panel_a/fig4a_FRTE_script.m
+++ Data_generation/Fig4/panel_a/fig4a_FRTE_script.m
@@ -1,7 +1,7 @@
 % fig 4a
 % initialising parameters
 
-A=100; M=10; T=0.1; C=0.35; beta1=3; beta2=0.01; lambda21=67/1520000; lambda12=0.00035/4; mu=0.00035; delta=0.007; NEVOL=3500; f0=0.002; m0=2; alpha0=0.4; 
+A=100; M=10; T=0.1; C=0.35; beta1=3; beta2=0.01; lambda21=67/1064000; lambda12=0.0005/4; mu=0.0005; delta=0.005; NEVOL=3500; f0=0.002; m0=2; alpha0=0.4; 
 switching_environments=1; plasticity=0; return_traits=0; number_of_realisations=25; alphamax=1000;
 
 
