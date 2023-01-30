8b9592d79ae2065ad2d793f057e78c684e1b3bc0
Xiaoyuan Liu <72927467+phil-liu1020@users.noreply.github.com>
2023-01-30 15:47:57 +0000

    Create FigS6.m

---
+++ Figure_generation/FigS6.m
@@ -0,0 +1,15 @@
+% Instructions for producing FigS6.
+
+% First, open "Data_Files/FigS6" folder.
+
+% To reproduce all the panels of FigS6, execute the command
+  
+Evol_Branching_plots(StrainsData_m);
+
+figure
+
+Evol_Branching_plots(StrainsData_alpha);
+
+figure
+
+Evol_Branching_plots_Coevolution(StrainsData_alpha);
