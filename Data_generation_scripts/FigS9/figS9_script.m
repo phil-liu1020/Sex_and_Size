a6747a6b30b1a8cf9bc95e4381ec82bb6de99650
Xiaoyuan Liu <72927467+phil-liu1020@users.noreply.github.com>
2023-01-31 13:12:46 +0000

    Update figS9_script.m

--- Data_generation_scripts/FigS9/figS9_script.m
+++ Data_generation_scripts/FigS9/figS9_script.m
@@ -4,11 +4,11 @@
 A=100; M=10; T=0.1; C=0.35; beta1=3; beta2=0.01; lambda21=67/532; lambda12=1/4; mu=0.00035; delta=0.007; NEVOL=4000; f0=0.002; m0=2; alpha0=0.4; 
 switching_environments=1; plasticity=0; return_traits=1; number_of_realisations=1; alphamax=1000;
 
+addpath(genpath('Sex_and_Size-main'))
 
 [StrainsData_m,StrainsData_alpha,m,alpha,~,~]=Evolutionary_trajectories(number_of_realisations,m0,alpha0,A,M,T,C,beta1,beta2,lambda12,lambda21,mu,NEVOL,f0,delta,alphamax, switching_environments, plasticity, return_traits );
 
 
-addpath(genpath('Sex_and_Size-main'))
 
 cd ..
 cd ..
