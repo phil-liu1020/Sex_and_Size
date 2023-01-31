b5fcf0a76533ffc90ece38a5f65f517126b2bd0e
Xiaoyuan Liu <72927467+phil-liu1020@users.noreply.github.com>
2023-01-31 10:25:46 +0000

    Add files via upload

---
+++ Data_generation_scripts/Fig3/fig3_script.m
@@ -0,0 +1,18 @@
+% fig 3
+% initialising parameters
+
+A=100; M=1; T=1; C=0.6; beta1=1; beta2=1; lambda12=0; lambda21=0; mu=0.001; delta=0.01; NEVOL=3000; f0=0.002; m0=1.5; alpha0=0.6;  switching_environments=0; plasticity=0; return_traits=1; number_of_realisations=1; alphamax=1000;
+
+addpath(genpath('Sex_and_Size-main'))
+
+[StrainData_m,StrainData_alpha,m,alpha,~,~]=Evolutionary_trajectories(number_of_realisations,m0,alpha0,A,M,T,C,beta1,beta2,lambda12,lambda21,mu,NEVOL,f0,delta,alphamax, switching_environments, plasticity, return_traits );
+
+
+cd ..
+cd ..
+
+
+save('Data_files\Fig3\m.mat','m');
+save('Data_files\Fig3\alpha.mat','alpha');
+save('Data_files\Fig3\StrainData_m.mat','StrainData_m');
+save('Data_files\Fig3\StrainData_alpha.mat','StrainData_alpha');
---
+++ Figure_generation_scripts/FigS2_to_S4_script.m
@@ -0,0 +1,25 @@
+% Instructions for producing FigS2 to FigS4
+
+% FigS2
+
+mstrains=[0,0.4,0.395]; alphastrains=[0,0.1,0.1]; S=2; beta=1; C=0.6; G=4000; T=1; A=100; M=1; f=[0.998,0.002];
+
+[g,~] = Plotting_Invasion_dynamics(mstrains,alphastrains,S,beta,C,G,T,A,M,f);
+
+plot(g(2,:))
+
+% FigS3
+
+mstrains=[0,0.4,0.4]; alphastrains=[0,0.1,0.105]; S=2; beta=1; C=0.6; G=15000; T=1; A=100; M=1; f=[0.998,0.002];
+
+[g,~] = Plotting_Invasion_dynamics(mstrains,alphastrains,S,beta,C,G,T,A,M,f);
+
+plot(g(2,:))
+
+% FigS4
+
+mstrains=[0,0.3,0.305]; alphastrains=[0,0.1,0.1]; S=2; beta1=4; beta2=0.01; lambda12=13/222; lambda21=1/6; C=0.7; G=600; T=0.1; A=100; M=10; f=[0.998,0.002];
+
+Invtraj=Plotting_Invasion_dynamics_SwitchingEnv(mstrains,alphastrains,S,beta1,beta2,lambda21,lambda12,C,G,T,A,M,f,stochastic);
+
+plot(Invtraj)
