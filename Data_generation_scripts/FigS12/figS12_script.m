ddf2568b7f1772c66613c4d85f6c9c1a6ebafb07
Xiaoyuan Liu <72927467+phil-liu1020@users.noreply.github.com>
2023-02-02 14:25:53 +0000

    Add files via upload

---
+++ Data_generation_scripts/FigS12/figS12_script.m
@@ -0,0 +1,22 @@
+% fig S12
+% initialising parameters
+
+A=100; M=10; T=0.1; C=0.7; beta1=4; beta2=0.01; lambda12=13/444000; lambda21=1/12000; mu=0.0005; delta=0.005; NEVOL=4000; f0=0.002; m0=2; alpha0=0.6; 
+switching_environments=1; plasticity=0; return_traits=1; number_of_realisations=1; alphamax=1000;
+
+cd ..
+cd ..
+addpath(genpath('Simulation_Functions'))
+cd Data_generation_scripts/FigS12
+
+[StrainsData_m,StrainsData_alpha,m,alpha,~,~]=Evolutionary_trajectories(number_of_realisations,m0,alpha0,A,M,T,C,beta1,beta2,lambda12,lambda21,mu,NEVOL,f0,delta,alphamax, switching_environments, plasticity, return_traits );
+
+
+
+cd ..
+cd ..
+
+save('Data_files\FigS12\m.mat','m');
+save('Data_files\FigS12\alpha.mat','alpha');
+save('Data_files\FigS12\StrainsData_m.mat','StrainsData_m');
+save('Data_files\FigS12\StrainsData_alpha.mat','StrainsData_alpha');
