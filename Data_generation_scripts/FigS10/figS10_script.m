c3691e1a6d2bc561381082208df76c480433c111
Xiaoyuan Liu <72927467+phil-liu1020@users.noreply.github.com>
2023-02-02 14:18:17 +0000

    Update figS10_script.m

--- Data_generation_scripts/FigS10/figS10_script.m
+++ Data_generation_scripts/FigS10/figS10_script.m
@@ -1,7 +1,7 @@
 % fig S10
 % initialising parameters
 
-A=100; M=10; T=0.1; C=0.35; beta1=3; beta2=0.01; lambda21=67/1064000; lambda12=1/8000; mu=0.0005; delta=0.005; NEVOL=4000; f0=0.002; m0=2; alpha0=0.4; 
+A=100; M=10; T=0.1; C=0.35; beta1=3; beta2=0.01; lambda12=1/8000; lambda21=67/1064000; mu=0.0005; delta=0.005; NEVOL=4000; f0=0.002; m0=2; alpha0=0.4; 
 switching_environments=1; plasticity=0; return_traits=1; number_of_realisations=1; alphamax=1000;
 
 cd ..
